a13308e94e443a4a3808eac21c99ae6840dabf69
Drew Maichel <59732004+troubleshoot@users.noreply.github.com>
2021-03-01 20:58:41 -0800

    Add files via upload

---
+++ Javascript/Algorithm_Practice.js
@@ -0,0 +1,26 @@
+// for(var i = 1; i < 20; i++){
+//   console.log(i);
+// }
+// 1
+// 3
+// 5
+// 7
+// 9
+// 11
+// 13
+// 15
+// 17
+// 19
+
+// var num = 0;
+// var sum = 0;
+// for(var i = 1; i <= 5; i++){
+//   num = i;
+//   sum = sum + num;
+//   console.log("Num: " + num + ", Sum: " + sum);
+// }
+// Num: 1, Sum: 1
+// Num: 2, Sum: 3
+// Num: 3, Sum: 6 
+// Num: 4, Sum: 10
+// Num: 5, Sum: 15
