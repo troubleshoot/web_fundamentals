c692cf2a56ff747b7357bb4d31a2306c99ec2221
Drew Maichel <59732004+troubleshoot@users.noreply.github.com>
2021-03-01 20:25:56 -0800

    Add files via upload

---
+++ Algos/3_1_algos.js
@@ -0,0 +1,30 @@
+// #1
+// for(var i = 0; i <= 10; i++){
+//  console.log(i);
+// }
+
+//#1-STEP UP
+// for(var i = 0; i <= 10; i++){
+//   //console.log(i);
+//   for(var j = 0; j < 5; j++){
+//     console.log(j);
+//   }
+// }
+
+//#2
+// function whatsInside(arr){
+//   for(var i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+//   }
+// }
+// whatsInside([1,2,3,4,5,6,7]);
+
+//#CHALLENGE
+// function repeatPrint(arr){
+//   for(var i = 0; i < arr.length; i++){
+//     for(var j = 0; j < arr.length; j++){
+//       console.log(arr[j]);
+//     }
+//   }
+// }
+// repeatPrint([1,2,3]);
