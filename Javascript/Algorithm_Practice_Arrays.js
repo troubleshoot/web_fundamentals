d48e6ad9dc813664ed1e409ef2b6d21c1165d4e7
Drew Maichel <59732004+troubleshoot@users.noreply.github.com>
2021-03-02 22:33:57 -0800

    Add files via upload

---
+++ Javascript/Algorithm_Practice_Arrays.js
@@ -0,0 +1,19 @@
+var testArr = [6,3,5,1,2,4]
+
+// var sum = 0;
+// for(var i = 0; i < testArr.length; i++){
+//   sum = testArr[i] + sum;
+//   console.log("Num: " + testArr[i] + ", Sum: " + sum);
+// }
+// Num 6, Sum 6
+// Num 3, Sum 9
+// Num 5, Sum 14
+// Num 1, Sum 15
+// Num 2, Sum 17
+// Num 4, Sum 21
+
+// var newArr = [];
+// for(var i = 0; i < testArr.length; i++){
+//   newArr[i] = testArr[i] * i;
+// }
+// console.log(newArr);
