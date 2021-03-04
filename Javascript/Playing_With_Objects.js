629e27948244ef5c0e660dfa488c7aeb035105f4
Drew Maichel <59732004+troubleshoot@users.noreply.github.com>
2021-03-04 12:37:48 -0800

    Add files via upload

---
+++ Javascript/Playing_With_Objects.js
@@ -0,0 +1,18 @@
+var users = [
+    {
+        name: "Michael",
+        age: 37
+    },
+    {
+        name: "John",
+        age: 30
+    },
+    {
+        name: "David",
+        age: 27
+    }
+];
+
+console.log(users[0].name + " - " + users[0].age);
+console.log(users[1].name + " - " + users[1].age);
+console.log(users[2].name + " - " + users[2].age);
